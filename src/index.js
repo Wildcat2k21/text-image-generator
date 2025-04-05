b4bbefbd53a2a22d685057bf6c96f49d9d3a6f23
andlitt <andreilitwinowwwwwwww@gmail.com>
2025-04-05 19:57:44 +0300

    исправлены баги, теперь текст выводится как строка, к нему корректно подключаются словари

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import generateRandomText from "./modules/generateRandomText";
-import { dictonaryRusLower } from "./constants/dictonaries";
+import { dictonaryLatLower } from "./constants/dictonaries";
 
-const test = generateRandomText(dictonaryRusLower, 10);
+const test = generateRandomText(dictonaryLatLower, 10);
+
 
 const isString = typeof test === "string";
 const isLengthIsTen = test.length === 10;
