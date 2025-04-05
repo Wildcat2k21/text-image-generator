aba9b0832a672407f39de62b73d82e1143ffd94a
wildcat2k21 <djerse12@mail.ru>
2025-04-06 00:17:42 +0300

    mini react logic component

---
+++ src/components/Options.js
@@ -0,0 +1,25 @@
+import { Component } from "../utils/Component.js";
+import { toggleTheme, themeInit } from "../utils/ThemeManager.js";
+ 
+export default function Options() {
+    return Component({
+        html: /*html*/ `
+            <div class="page-theme">
+                <h3>Настройки страницы</h3>
+                <label class="page-theme__label">
+                    Темная тема
+                    <input class="page-theme__checkbox" type="checkbox" id="dark-theme-checkbox">
+                </label>
+            </div>
+        `,
+        setup: (el) => {
+            const checkbox = el.querySelector("#dark-theme-checkbox");
+            checkbox.addEventListener("change", (e) => {
+                toggleTheme(e.target.checked);
+            });
+
+            // Инициализируем тему из localStorage
+            checkbox.checked = themeInit();;
+        }
+    });
+}
