46ef64edbfd9504b426df8b9f837cb95e6ed0596
wildcat2k21 <djerse12@mail.ru>
2025-04-05 16:14:36 +0300

    exports demo for begin project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import generateRandomText from "./modules/generateRandomText";
+import { dictonaryRusLower } from "./constants/dictonaries";
+
+const test = generateRandomText(dictonaryRusLower, 10);
+
+const isString = typeof test === "string";
+const isLengthIsTen = test.length === 10;
+
+console.log(test, isString, isLengthIsTen, "То или не то: ", isString && isLengthIsTen);
 console.log("hello world");
