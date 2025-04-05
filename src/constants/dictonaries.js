3319264072a80aa0a65fdebd6111586177256e4d
andlitt <andreilitwinowwwwwwww@gmail.com>
2025-04-05 15:06:07 +0300

    модуль для генерации случайного текста и словари

---
+++ src/constants/dictonaries.js
@@ -0,0 +1,11 @@
+//Cловари для генерации текста
+
+export const dictonaryRusLower = ["абвгдежзийклмнопрстуфхцчшщъыьэюя"];
+
+export const dictonaryRusUpper = ["АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"];
+
+export const dictonaryLatLower = ["abcdefghijklmnopqrstuvwxyz"];
+
+export const dictonaryLatUpper = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+
+export const dictonaryNumbers  = ["0123456789"];
