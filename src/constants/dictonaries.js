0af2ed1dc3a51655e8621ca77dec20d9e3015b9c
andlitt <andreilitwinowwwwwwww@gmail.com>
2025-04-05 19:57:44 +0300

    исправлены баги, теперь текст выводится как строка, к нему корректно подключаются словари

--- src/constants/dictonaries.js
+++ src/constants/dictonaries.js
@@ -1,11 +1,11 @@
 //Cловари для генерации текста
 
-export const dictonaryRusLower = ["абвгдежзийклмнопрстуфхцчшщъыьэюя"];
+export const dictonaryRusLower = "абвгдежзийклмнопрстуфхцчшщъыьэюя";
 
-export const dictonaryRusUpper = ["АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"];
+export const dictonaryRusUpper = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ";
 
-export const dictonaryLatLower = ["abcdefghijklmnopqrstuvwxyz"];
+export const dictonaryLatLower = "abcdefghijklmnopqrstuvwxyz";
 
-export const dictonaryLatUpper = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+export const dictonaryLatUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-export const dictonaryNumbers  = ["0123456789"];
+export const dictonaryNumbers  = "0123456789";
--- src/modules/generateRandomText.js
+++ src/modules/generateRandomText.js
@@ -5,7 +5,7 @@ import { getRandomSign } from "../helpers/dictonaries.js";
 
 //основная функция для генерации случайного текста(определённой длины length) из словаря - dictonary
 export default function generateRandomText(dictonary, length){
-    const text = [];
+    let text = "";
     const punctuationMarks = [".","?","!",",",":","-"];
     
     for(let i = 0; i < length; ++i){
@@ -17,13 +17,13 @@ export default function generateRandomText(dictonary, length){
             if(i === 0 || (text[i-1] === "." || text[i-1] === "?" || text[i-1] === "!")) {
                 word = word.charAt(0).toUpperCase();
             }            
-            text.push(word);
+            text += word;
         }
         else if(i > 0 && 0.2 > rand && rand >= 0.15){
-            text.push(punctuationMarks[getRandomSign(punctuationMarks)]);
+            text += punctuationMarks[getRandomSign(punctuationMarks)];
         }
         else if(i > 0 && 0.15 > rand && rand >= 0){            
-            text.push(" ");        
+            text += " ";        
         }          
     }
     // console.log(text) 
