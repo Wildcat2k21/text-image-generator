7cd14e0f194ef5eb3752086ff8808592084ee029
wildcat2k21 <djerse12@mail.ru>
2025-04-06 23:34:26 +0300

    косметическая правка шаблона html

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
 import "./index.css";
 import Options from "./components/Options.js";
-import TextCanvas from "./components/P5Canvases/TextCanvas.js";
+import Manuscript from "./components/P5_sketches/Manuscript.js";
 
 const app = document.querySelector(".app");
-
 const main = document.createElement("main");
 main.classList.add("app__content");
 
 main.innerHTML = /*html*/ `
     <h1 class="app__title">Генератор текстовых изображений</h1>
     <div class="app__options"></div>
-    <div class="sketch-container"></div>
+    <div class="p5-sketch-container">
+        <!-- скетчи вставляются здесь -->
+    </div>
 `;
 
 main.querySelector(".app__options").appendChild(Options());
-main.querySelector(".sketch-container").appendChild(TextCanvas());
+main.querySelector(".p5-sketch-container").appendChild(Manuscript());
 app.appendChild(main);
